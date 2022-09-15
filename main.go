ba930b83bff17bf34eb7aad2c33bb632032402a3
Nzyazin <69486560+Nzyazin@users.noreply.github.com>
2022-09-15 22:25:03 +0300

    create and

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ const (
 	dbname   = "library"
 )
 
-func main() {
+func create(a_id int, a_name string, b_day string) {
 	psqlconn := fmt.Sprintf("host= %s port = %d user = %s password =%s dbname = %s sslmode=disable", host, port, user, password, dbname)
 
 	db, err := sql.Open("postgres", psqlconn)
@@ -23,18 +23,68 @@ func main() {
 		fmt.Println("Could not connect", err)
 	}
 	defer db.Close()
+	stmt, err := db.Prepare("INSERT INTO author(author_id, author_name, birthday) VALUES($1, $2, $3)")
+	if err != nil {
+		log.Fatal("Here2: ", err)
+	}
+
+	res, err := stmt.Exec(a_id, a_name, b_day)
+	if err != nil {
+		log.Fatal("Here1: ", err)
+	}
+
+	rowCnt, err := res.RowsAffected()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("ID = %d, affected = %d\n Succeded!!!", res, rowCnt)
+}
+
+func main() {
+	variable := true
+	for variable {
+		var operation int
+		fmt.Println("Input a number for procedure with database or for quit: \n1. Create a field in table. \n2. Read a table. \n3. Update a field in table. \n4. Delete a field. \n5. Quit")
+		fmt.Scanln(&operation)
+		switch operation {
+		case 1:
+			fmt.Println("Input author id:,  and his birthday:")
+			var authorId int
+			fmt.Println("Input author author name:")
+			var name string
+			fmt.Println("And his birthday:")
+			var birthday string
+			fmt.Scanln(&authorId, &name, &birthday)
+			create(authorId, name, birthday)
+		case 5:
+			variable = false
+			fmt.Println("Bye bye")
+		}
+	}
+	psqlconn := fmt.Sprintf("host= %s port = %d user = %s password =%s dbname = %s sslmode=disable", host, port, user, password, dbname)
+
+	db, err := sql.Open("postgres", psqlconn)
+	if err != nil {
+		fmt.Println("Could not connect", err)
+	}
+	defer db.Close()
+
 	var (
 		authorId int
-		name     string
-		birthDay int
+		name     sql.NullString
+		birthday sql.NullString
 	)
-	rows, err := db.Query("INSERT INTO author(author_id, author_name, birthday) values ('10', 'Petrov K. P.', 'birthday')", 1)
+	rows, err := db.Query("select * from author")
+	if err != nil {
+		log.Fatal("Did`not query:", err)
+	}
+
 	for rows.Next() {
-		err := rows.Scan(&authorId, &name, &birthDay)
+		err := rows.Scan(&authorId, &name, &birthday)
 		if err != nil {
-			log.Fatal(err)
+			log.Fatal("Did`not connect", err)
 		}
-		log.Println(authorId, name, birthDay)
+		log.Println(authorId, name.String, birthday.String)
 	}
 	if err != nil {
 		fmt.Println("Can not:", err)
